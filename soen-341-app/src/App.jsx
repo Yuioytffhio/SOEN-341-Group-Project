ebb086f6b0d4faa3409cf0fc838b2911a14e7abb
Nadine-seb <Nadine.s1123@gmail.com>
2025-10-02 12:14:14 -0400

    I created the navbar but still have to work on it

---
+++ soen-341-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './components/navbar/Navbar'
+
+
+const App = () => {
+  return (
+    <div>
+        <div className='container'>
+            <Navbar/>
+        </div>
+    </div>
+  )
+}
+
+export default App
---
+++ soen-341-app/src/components/navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <img src=" " alt="" classname ='logo'/>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Discovery</a></li>
+            <li><a href="#">My events</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Profile (icon)</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
+
+// git add .
+// git commit -m "blah blah blah"
+// git push origin navbar
