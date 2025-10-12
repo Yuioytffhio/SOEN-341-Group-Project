ed5d68784e8ca84e87ff6c6d801178b884373c7f
HibaT <h_talbi@live.concordia.ca>
2025-10-12 17:06:13 -0400

    Student Home Page fixed, issue #28

--- frontend/src/pages/student/HomePageStudent/HomePageStudent.jsx
+++ frontend/src/pages/student/HomePageStudent/HomePageStudent.jsx
@@ -4,22 +4,25 @@ import Slider from "react-slick";
 import "./HomePageStudent.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import HomePageBackground2 from "../../../assets/homepage_caroussel2.jpg";
+import HomePageBackground3 from "../../../assets/homepage_caroussel3.jpg";
 
 export default function StudentHomePage() {
   const navigate = useNavigate();
   const userName = localStorage.getItem("userName") || "Student";
 
   const eventImages = [
-    "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=800&q=80",
-    "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&w=800&q=80",
+    "https://images.unsplash.com/photo-1504384308090-c894fdcc538d?auto=format&fit=crop&w=2400&q=100",
+    "https://images.unsplash.com/photo-1492684223066-81342ee5ff30?auto=format&fit=crop&w=2400&q=100",
+    HomePageBackground3,
+    HomePageBackground2
   ];
 
   const settings = {
     dots: true,
     infinite: true,
     speed: 600,
-    slidesToShow: 2,
+    slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2500,
@@ -32,27 +35,24 @@ export default function StudentHomePage() {
         <h2 className="greetings">Welcome back, {userName}!</h2>
       </header>
 
-      <div className="student-main-content">
-        <div className="top-section">
+      <div className="carousel-section">
+        <Slider {...settings}>
+          {eventImages.map((src, index) => (
+            <div key={index} className="carousel-slide">
+              <img src={src} alt={`Event ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+
+        <div className="overlay">
           <p>You can start browsing exciting campus events right away.</p>
-          <button className="discover-btn" onClick={() => navigate("/discovery")}>
+          <button
+            className="discover-btn"
+            onClick={() => navigate("/discovery")}
+          >
             Discover Events
           </button>
         </div>
-
-        <div className="bottom-section">
-          <div style={{ width: "100vh", maxWidth: "100vh" }}>
-            <Slider {...settings}>
-              {eventImages.map((src, index) => (
-                <div key={index} className="carousel-slide">
-                  <img src={src} alt={`Event ${index + 1}`} />
-                </div>
-              ))}
-            </Slider>
-          </div>
-        </div>
-
-
       </div>
     </div>
   );
