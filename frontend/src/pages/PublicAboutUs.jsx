0f5dec535d0a5b64ce36ceb4edff52e62078a194
Yuioytffhio <hiba.tal05@gmail.com>
2025-11-04 16:02:16 -0500

    Fixed about us page bug and added link to homepage on navbar, #44

---
+++ frontend/src/pages/PublicAboutUs.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/PublicAboutUs.css";
+
+const AboutUs = () => {
+  return (
+    <div className="aboutus-page">
+      <header className="aboutus-header">
+        <h2 className="greetings">About us</h2>
+      </header>
+
+       <div className="aboutus-content">
+        <p>
+          Welcome to our campus ticketing event platform, designed by Concordia University software and computer engineering students.
+        </p>
+
+        <p>
+          Our mission is to make it easier for students, organizers, and administrators to browser through the different events available on campus.
+          discover, manage, and host events all in one place. Whether you're looking to join a club activity, attend a talk, or promote your own event, CampusConnect helps you stay engaged with everything happening around you.
+        </p>
+
+        <p>
+          This project was developed as part of the SOEN 341 course. 
+        </p>
+        
+        <h3>Our Team</h3>
+        <p>Nadine Seba  Raphael Procopi Hiba Talbi  Marc Espin  Lojaine Matar   Patel Shyam Siam Manwar Pranto  Thomas Sullivan</p>
+
+        <h3>Technologies Used</h3>
+        <p>React, Firebase, Node.js.</p>
+
+        <div className="team-section">
+          
+        </div>
+
+        <div className="tech-section">
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
